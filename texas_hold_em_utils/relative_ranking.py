4d4d4fbf0fa0eb27978002b4b72fc22cf968d5f5
Alex Markules <amarlar2354@gmail.com>
2024-10-01 20:42:16 -0400

    get_hand_stats() for multiple players

--- texas_hold_em_utils/relative_ranking.py
+++ texas_hold_em_utils/relative_ranking.py
@@ -4,7 +4,9 @@ from texas_hold_em_utils.deck import Deck
 
 def rank_hand(hand, community_cards):
     """
+    *Post Flop*
     Ranks a hand of two cards and 3-5 five community cards relative to all other possible hands, based only on known cards
+    Known issue: odds assume no additional community cards. Works best for after the river, does an ok approx before then
     :param hand: a list of 2 cards (Card objects)
     :param community_cards: a list of 3-5 cards (Card objects)
     :return: a tuple of the number of wins, losses, and ties for the given hand
@@ -27,3 +29,23 @@ def rank_hand(hand, community_cards):
                     else:
                         ties += 1
     return wins, losses, ties
+
+
+def get_hand_stats(hand, community_cards, player_count):
+    """
+    *Post Flop*
+    Provides expected outcome information for a given hand based on the number of players
+    Known issue: odds assume no additional community cards. Works best for after the river, does an ok approx before then
+    :param hand: 2 cards
+    :param community_cards: a list of 3-5 cards
+    :param player_count: 2+
+    :return: dict with keys: "two_player_effective_win_rate", "percentile", "win_rate"
+    """
+    wins, losses, ties = rank_hand(hand, community_cards)
+    two_player_effective_win_rate = ((2 * wins) + ties) / (
+                2 * (wins + losses + ties)) if wins + losses + ties > 0 else 0
+    percentile = two_player_effective_win_rate * 100
+    # win rate is the chance that the hand beats player_count other hands
+    win_rate = two_player_effective_win_rate ** (player_count - 1)
+    return {"two_player_effective_win_rate": two_player_effective_win_rate, "percentile": percentile,
+            "win_rate": win_rate}
